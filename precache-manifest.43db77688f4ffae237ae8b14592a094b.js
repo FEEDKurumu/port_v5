3c05190de05e9fdf4624c2ca0c8efef07a741c37
FEEDKurumu <xdjlee@gmail.com>
2019-03-25 12:35:37 -0700

    Updates

---
+++ precache-manifest.43db77688f4ffae237ae8b14592a094b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/port_v5/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "daaff9f4ca4843c74378",
+    "url": "/port_v5/static/js/runtime~main.eed3373a.js"
+  },
+  {
+    "revision": "03a29e4afe77e9787004",
+    "url": "/port_v5/static/js/main.eb7199f4.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/port_v5/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "03a29e4afe77e9787004",
+    "url": "/port_v5/static/css/main.a6c2db9a.chunk.css"
+  },
+  {
+    "revision": "206c690701b6fbadf2c1d0fcfc63aea9",
+    "url": "/port_v5/index.html"
+  }
+];
